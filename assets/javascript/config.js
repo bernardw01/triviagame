d488ff3b775abe642a68370915cbc19312686a4b
Bernard Williams <bernard.l.williams@live.com>
2017-06-01 20:33:50 -0400

    Continued development

---
+++ assets/javascript/config.js
@@ -0,0 +1,48 @@
+/* 
+ Bernard Williams
+ UCF Bootcamp APR2017 Week 5
+ Trivia Game
+ */
+
+var questionBank = [
+    {
+        question: "Orlando is home to how many lakes?",
+        answers: [
+            'More than 100',
+            'Between 20 and 50',
+            'Between 50 and 75',
+            'Between 75 and 100'
+        ],
+        rightAnswerID: 0,
+        funFact: 'Its not called the Land-o-lakes for nothing',
+        used: false
+
+    },
+    {
+        question: "The Citrus Bowl, currently known as Campers World Stadium was originally built in what year?",
+        answers: [
+            '1945',
+            '1972',
+            '1984',
+            '1936'
+        ],
+        rightAnswerID: 3,
+        funFact: 'It originally had a seating capacity of 10,000 people.',
+        used: false
+
+    },
+    {
+        question: "What building in Orlando was blown up during the filming of Lethal Weapon 3?",
+        answers: [
+            'The Original Citrus Bowl',
+            'City Hall',
+            'The Rogers Building',
+            'The original Amway Arena'
+        ],
+        rightAnswerID: 1,
+        funFact: 'The mayor at the time did a cameo appearance in the movie right after.',
+        used: false
+
+    },
+]
+
